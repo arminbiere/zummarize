4bdd052677c4db331447eb54b2578c5c113e8a91
Armin Biere <biere@jku.at>
2015-06-01 13:40:43 +0200

    compiles

--- zummarize.c
+++ zummarize.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+
+static void die (const char * fmt, ...) {
+  va_list ap;
+  fputs ("*** zummarize: ", stderr);
+  va_start (ap, fmt);
+  vfprintf (stderr, fmt, ap);
+  va_end (ap);
+  fputc ('\n', stderr);
+  exit (1);
+}
+
+static const char * USAGE =
+"usage: zummarize [-h] [ <dir> ... ]\n"
+;
+
+static void usage () {
+  fputs (USAGE, stdout);
+  exit (0);
+}
+
+static int isdir (const char * path) {
+  return 1;
+}
 
 int main (int argc, char ** argv) {
+  int i, count = 0;
+  for (i = 1; i < argc; i++) {
+    if (!strcmp (argv[i], "-h")) usage ();
+    else if (argv[i][0] == '-') die ("invalid option '%s'", argv[i]);
+    else if (!isdir (argv[i])) die ("argument '%s' not a directory", argv[i]);
+    else count++;
+  }
+  if (!count) die ("no directory specified");
+  return 0;
 }
